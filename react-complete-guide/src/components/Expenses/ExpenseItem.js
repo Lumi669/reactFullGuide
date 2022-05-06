3b41e7f94aeb37334d0dd341a47a6129aa891f8c
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-06 10:17:03 +0300

    useState hook

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -6,9 +6,9 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-
+  console.log("apple");
   const clickHandler = () => {
-    setTitle("Updated");
+    setTitle("Updatppppped");
     console.log(title);
   };
   return (
@@ -30,5 +30,7 @@ Note:
 useState: 
 1. Used only directly inside component function, not in nested function
 2. the state only works inside this component, not other component.
+3. IMPORTANT !!! when we say the this component, means the specific component instance created. for example, if this ExpenseItem component is repeatedly used 4 times, then each time the ExpenseItem coponent is considered one component instance. The 1st is different from the 2nd, 3rd and 4th ExpenseItem components. The state in the 1st ExpenseItem component istance is confined only to the 1st ExpenseItem component instance, not other 3.
 
+Every instance receives its own state, and which is detached from other 3 instances.
 */
